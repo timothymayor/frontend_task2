b4f12c7ec662376fb5479c347a1ca88dfc6beed5
Timothy Mayor <timothymayor24@gmail.com>
2020-04-09 10:09:06 +0100

    modified file

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
-data = [{
-        "principal": 2500,
-        "time": 1.8
-    },
-    {
-        "principal": 1000,
-        "time": 5
-    },
-    {
-        "principal": 3000,
-        "time": 1
-    },
-    {
-        "principal": 2000,
-        "time": 3
-    }
-
-]
+var data = [
+        {"principal": 2500, "time": 1.8},
+        {"principal": 1000, "time": 5},
+        {"principal": 3000, "time": 1},
+        {"principal": 2000, "time": 3} 
+    ]
 
 
 const interestCalculator = (arr) => {
@@ -40,6 +28,5 @@ const interestCalculator = (arr) => {
     return interestData
 
 }
-
 
 interestCalculator(data);
